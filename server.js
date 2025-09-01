36e55dfdee1a4a9c94315abe68aaa2eb19fc7b9b
selfiction <cojarsenij@gmail.com>
2025-09-02 04:11:20 +0500

    added SSL

--- server.js
+++ server.js
@@ -2908,7 +2908,7 @@ const options = {
 
 https.createServer(options, app).listen(3000, () => {
   var os = require('os');
-  console.log(os.hostname());
+  os.hostname()
   console.log("HTTPS сервер запущен: https://localhost:3000");
 });
 
